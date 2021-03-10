ae64e05bd87a10df133da4322643b0f15ca4da76
James Walden <prussian1740@gmail.com>
2021-03-10 17:11:49 -0600

    calc and evenodd

---
+++ week_1/day_3/calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ week_1/day_3/evenodd.py
@@ -0,0 +1,7 @@
+number = int(input("Enter an integer number"))
+remainder = number % 2
+if (remainder == 0):
+    print(number, " is an even Number")
+else: 
+    print(number,"is an odd Number26")
+26
