6df0d1d272232a57f6a50279b8b6f95b9b5ff5b8
James Walden <prussian1740@gmail.com>
2021-03-10 10:33:50 -0600

    added python files

---
+++ week_1/day_3/pythonExamples.py
@@ -0,0 +1,30 @@
+# Variable
+# String === "", "word", "1", "functionName"
+# Integer === whole number, no decimals
+# Float === numbers with a decimal, rarely used
+# Boolean === true or false, true, false, returns a truthy or falsy value
+# Name, assignment operator (=), value that I am assigning
+# list, a sequence of values followed by [value1,value2,etc]
+#greeting = "Hello World"
+#classSize = 23
+#floatExample = 2.323
+#sumOfNumbers = 1 + 1
+#variableName = True
+#TRUTHY 
+#FALSY
+#print("string" == "string")
+#[1, 2, 3]
+#[False, True, True]
+#["string", 1, 3.4, True, "string"]
+# Nesting
+#["list", ["list"]]
+# Dictionary
+# Sequence of labeled values {key: value}
+#{"stringName": "StringValue"}
+#{"FavoriteColor": "Red"}
+print("Hello World")
+print("My name is James and I am in Atlanta")
+greeting = "Hello!"
+classSize = 23
+print(greeting)
+print(classSize)
