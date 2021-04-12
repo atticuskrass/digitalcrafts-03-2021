b7a3982226a2e0768385017e19d8d2b6517fa3a7
James Walden <prussian1740@gmail.com>
2021-04-12 16:21:41 -0400

    LOL

--- week_6/frontendproject/JavaScript/culture.js
+++ week_6/frontendproject/JavaScript/culture.js
@@ -2,7 +2,7 @@
 const getNews1 = async () => {
 	//-Get News Data-//
 	const getNewsData = await fetch(
-		"https://api.thenewsapi.com/v1/news/all?api_token=6i1mPY54dyfuIGs0MchnCRkl63fBulfmFaqjtCrh&language=en&limit=5"
+		"https://api.thenewsapi.com/v1/news/all?api_token=6i1mPY54dyfuIGs0MchnCRkl63fBulfmFaqjtCrh&language=en&categories=entertainment,food,travel&limit=5"
 	);
 	const getNewsDataJson = await getNewsData.json();
 	console.log(getNewsDataJson);
@@ -18,10 +18,10 @@ const getNews1 = async () => {
 		let articleTitle = getNewsDataJson.data[counter].title;
 		let articleDescription = getNewsDataJson.data[counter].description;
 		let articleSnippet = getNewsDataJson.data[counter].snippet;
-		let articleSource = getNewsDataJson.data[counter].url;
+		// let articleSource = getNewsDataJson.data[counter].url;
 		let articleImage = getNewsDataJson.data[counter].image_url;
 		//-Article Header-//
-		const articleHeader = document.createElement("h2");
+		const articleHeader = document.createElement("h3");
 		articleHeader.className = "articleHeader";
 		articleHeader.innerText = `${articleTitle}`;
 		articleDiv.append(articleHeader);
@@ -34,17 +34,19 @@ const getNews1 = async () => {
 		articleDescriptItem.innerText = `${articleDescription}`;
 		articleCardList.append(articleDescriptItem);
 		const articleSnippetItem = document.createElement("li");
-		articleSnipperItem.className = "articleSnippetItem";
+		articleSnippetItem.className = "articleSnippetItem";
 		articleSnippetItem.innerText = `${articleSnippet}`;
 		articleCardList.append(articleSnippetItem);
-		const articleSourceItem = document.createElement("li");
-		articleSourceItem.className = "articleSourceItem";
-		articleSourceItem.innerText = `${articleSource}`;
-		articleCardList.append(articleSourceItem);
-		const articleImageItem = document.createElement("img");
-		articleImageItem.className = "articleImageItem";
-		articleImageItem.src = `${articleImage}`;
-		articleCardList.append(articleImageItem);
+		// const articleSourceItem = document.createElement("li");
+		// articleSourceItem.className = "articleSourceItem";
+		// articleSourceItem.innerText = `${articleSource}`;
+		// articleCardList.append(articleSourceItem);
+		// const articleImageItem = document.createElement("img");
+		// articleImageItem.className = "articleImageItem";
+		// articleImageItem.src = `${articleImage}`;
+		// articleImageItem.height = "300";
+		// articleImageItem.width = "400";
+		// articleCardList.append(articleImageItem);
 		counter += 1;
 	}
 };
---
+++ week_6/frontendproject/JavaScript/exhibitors.js
@@ -0,0 +1,60 @@
+//--Companies Buttons--//
+
+const submitButton = document.createElement("button");
+submitButton.id = "submitButton";
+submitButton.type = "input";
+submitButton.innerText = "Get Companies";
+headerDiv.append(submitButton);
+const clearButton = document.createElement("button");
+clearButton.id = "clearButton";
+clearButton.type = "input";
+clearButton.innerText = "Clear Page";
+headerDiv.append(clearButton);
+
+//--Get Exhibitors Functions--//
+const getExhibitors = async () => {
+	//--Fetch Exhibitor Data--//
+	const getExhibitorData = await fetch(
+		"https://fakerapi.it/api/v1/companies?_quantity=8"
+	);
+	const companyDataJson = await getExhibitorData.json();
+	//--Create Exhibitor Card(s)--//
+	let counter = 0;
+	for (let company of companyDataJson.data) {
+		//-Div-//
+		const companyDiv = document.createElement("div");
+		companyDiv.className = "companyDiv";
+		companyDiv.setAttribute("id", `company-${counter}`);
+		divContainer.append(companyDiv);
+
+		//-Element InnerText-//
+		let companyName = companyDataJson.data[counter].name;
+		let companyLocale = companyDataJson.data[counter].country;
+		let companyCity = companyDataJson.data[counter].addresses[0].city;
+		let companyWebsite = companyDataJson.data[counter].website;
+		let companyEmail = companyDataJson.data[counter].email;
+		let companyPhone = companyDataJson.data[counter].phone;
+
+		//-Company Name Header-//
+		const companyHeaderName = document.createElement("h3");
+		companyHeaderName.id = "companyHeaderName";
+		companyDiv.append(companyHeaderName);
+		companyHeaderName.innerText = `${companyName}`;
+
+		//-Company Card List-//
+		const companyCardList = document.createElement("ul");
+		companyCardList.id = "companyCardList";
+		companyDiv.append(companyCardList);
+		const companyListItem = document.createElement("li");
+		companyCardList.append(companyListItem);
+		companyListItem.innerText = `Headquarters located in ${companyCity}, ${companyLocale}`;
+		const companyContactItem = document.createElement("li");
+		companyCardList.append(companyContactItem);
+		companyContactItem.innerText = `Can be contacted at ${companyEmail} or ${companyPhone}`;
+		const companyWebsiteItem = document.createElement("li");
+		companyCardList.append(companyWebsiteItem);
+		companyWebsiteItem.innerText = `Company website is ${companyWebsite}`;
+		counter += 1;
+	}
+};
+console.log(getExhibitors());
