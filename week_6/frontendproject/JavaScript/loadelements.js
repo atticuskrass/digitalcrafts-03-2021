2b510779112b103fb5325394624fab9e47bc7b04
James Walden <prussian1740@gmail.com>
2021-04-11 20:58:24 -0400

    modified project

--- week_6/frontendproject/JavaScript/loadelements.js
+++ week_6/frontendproject/JavaScript/loadelements.js
@@ -1,27 +1,41 @@
 //----Repeat HTML Elements----//
 
-//--Container Div--//
-const containerDiv = document.createElement("div");
-containerDiv.id = "containerDiv-1";
-document.body.appendChild(containerDiv);
 //--Header/Navigation Div--//
 const headerDiv = document.createElement("div");
 headerDiv.id = "headerDiv-1";
 document.body.appendChild(headerDiv);
+//--Container Div--//
+const containerDiv = document.createElement("div");
+containerDiv.id = "containerDiv-1";
+document.body.appendChild(containerDiv);
 //--Navigation Links--//
 const anchorHome = document.createElement("a");
-anchorHome.setAttribute("href", "../Home/home.html");
+anchorHome.setAttribute("href", "home.html");
 anchorHome.innerText = "Home";
 anchorHome.id = "anchorHome";
 headerDiv.append(anchorHome);
 const anchorNews = document.createElement("a");
-anchorNews.setAttribute("href", "../News/news.html");
+anchorNews.setAttribute("href", "news.html");
 anchorNews.innerText = "News";
 anchorNews.id = "anchorNews";
 headerDiv.append(anchorNews);
 const anchorCulture = document.createElement("a");
-anchorCulture.setAttribute("href", "..Culture/culture.html");
-anchorCulture.innerText = "Culture and Art";
+anchorCulture.setAttribute("href", "culture.html");
+anchorCulture.innerText = "Culture";
 anchorCulture.id = "anchorCulture";
 headerDiv.append(anchorCulture);
-const anchorMarket = document.createElement("a");
+const anchorSport = document.createElement("a");
+anchorSport.setAttribute("href", "sport.html");
+anchorSport.innerText = "Sport";
+anchorSport.id = "anchorSport";
+headerDiv.append(anchorSport);
+const anchorCommerce = document.createElement("a");
+anchorCommerce.setAttribute("href", "market.html");
+anchorCommerce.innerText = "Commerce";
+anchorCommerce.id = "anchorCommerce";
+headerDiv.append(anchorCommerce);
+const anchorTech = document.createElement("a");
+anchorTech.setAttribute("href", "tech.html");
+anchorTech.innerText = "Technology";
+anchorTech.id = "anchorTech";
+headerDiv.append(anchorTech);
