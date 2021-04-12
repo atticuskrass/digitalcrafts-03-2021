82bb5cdeab45c5099ce4b53cefb08d5b89521589
James Walden <prussian1740@gmail.com>
2021-04-11 23:40:42 -0400

    frontend proj

--- week_6/frontendproject/JavaScript/news.js
+++ week_6/frontendproject/JavaScript/news.js
@@ -6,27 +6,38 @@ const inputCountry = document.createElement("input");
 inputCountry.id = "inputCountry";
 inputCountry.type = "text";
 inputCountry.placeholder = "Enter country";
-const inputDate = document.createElement("input");
-inputDate.id = "inputDate";
-inputDate.type = "text";
-inputDate.placeholder = "Enter From Date in 00-00-0000 Format";
+headerDiv.append(inputCountry);
+// const inputDate = document.createElement("input");
+// inputDate.id = "inputDate";
+// inputDate.type = "text";
+// inputDate.placeholder = "From Date in 00-00-0000";
+// headerDiv.append(inputDate);
 const inputOrganize = document.createElement("input");
 inputOrganize.id = "inputOrganize";
 inputOrganize.type = "text";
 inputOrganize.placeholder = "Select Relevancy, Popularity, publishedAt";
+headerDiv.append(inputOrganize);
 //-Buttons-//
 const submitButton = document.createElement("button");
 submitButton.id = "submitButton";
 submitButton.type = "input";
 submitButton.innerText = "Get News";
+headerDiv.append(submitButton);
 const clearButton = document.createElement("button");
 clearButton.id = "clearButton";
 clearButton.type = "input";
 clearButton.innerText = "Clear Page";
-// //--News Category Function--//
-// const newsCategory = async () => {};
-// //--Fetch News Data--//
-// const newsData = await fetch(
-// 	"https://newsapi.org/v2/top-headlines?country=us&category=world&apiKey=0ff4213a96f34bf6a9c611fd797edbf9"
-// );
-// const newsDataJson = newsData.json();
+headerDiv.append(clearButton);
+
+//--Get News Function--//
+const getNews = async () => {
+	//-Grab Input Values-//
+	const grabInputCountry = document.querySelector("#inputCountry").value;
+	const grabInputOrganize = document.querySelector("#inputOrganize").value;
+	//-Fetch News Data--//
+	const getNewsData = await fetch(
+		`https://newsapi.org/v2/everything?q=${grabInputCountry}&from=04-01-2021&sortBy=${grabInputOrganize}&pagesize=5&apiKey=0ff4213a96f34bf6a9c611fd797edbf9`
+	);
+	const newsDataJson = await getNewsData.json();
+	//-Create Article Div-//
+};
