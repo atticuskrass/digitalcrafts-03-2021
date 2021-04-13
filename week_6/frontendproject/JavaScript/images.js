68f5802f4dfb2e9c5f6538b6b0caa50eef6854fa
James Walden <prussian1740@gmail.com>
2021-04-12 23:08:48 -0400

    stuff

--- week_6/frontendproject/JavaScript/images.js
+++
@@ -1,8 +0,0 @@
-const getImages = async () => {
-	const getImageData = await fetch(
-		"https://picsum.photos/v2/list?page=2&limit=100"
-	);
-	const getImageDataJson = await getImageData.json();
-	console.log(getImageDataJson);
-};
-console.log(getImages());
