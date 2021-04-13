ccaaab7bb62e5220d2a3997afd58cf812c7abb56
James Walden <prussian1740@gmail.com>
2021-04-13 13:16:17 -0400

    working on div grids

--- week_6/frontendproject/JavaScript/index.js
+++ week_6/frontendproject/JavaScript/index.js
@@ -61,40 +61,50 @@ grabHomeLink.addEventListener("mousedown", () => {
 
 const getCreators = async () => {
 	//--Fetch Creator Data--//
-	const getCreatorData = await fetch("https://randomuser.me/api/?results=10");
+	const getCreatorData = await fetch("https://randomuser.me/api/?results=5");
 	const peopleDataJson = await getCreatorData.json();
 	console.log(peopleDataJson);
 	//--Featured Creator Card(s)--//
 	let counter = 0;
 	for (let creator of peopleDataJson.results) {
 		//-Div-//
+
 		const featuredCreatorDiv = document.createElement("div");
 		featuredCreatorDiv.className = "featuredCreatorDiv";
 		featuredCreatorDiv.setAttribute("id", `creator-${counter}`);
 		containerDiv.append(featuredCreatorDiv);
+
 		//-Element InnerText-//
+
 		let personFirstName = peopleDataJson.results[counter].name.first;
 		let personLastName = peopleDataJson.results[counter].name.last;
 		let personCity = peopleDataJson.results[counter].location.city;
 		let personCountry = peopleDataJson.results[counter].location.country;
 		let personEmail = peopleDataJson.results[counter].email;
 		let personPicture = peopleDataJson.results[counter].picture.large;
+
 		//-Creator Name Header-//
+
 		const personName = document.createElement("h3");
 		personName.id = "personName";
 		featuredCreatorDiv.append(personName);
 		personName.innerText = `${personLastName}, ${personFirstName}`;
+
 		//-Creator Card List-//
+
 		const personCardList = document.createElement("ul");
-		personCardList.id = "personCity";
+		personCardList.id = "personList";
 		featuredCreatorDiv.append(personCardList);
 		const cityListItem = document.createElement("li");
+		cityListItem.className = "cityListItem";
 		personCardList.append(cityListItem);
 		cityListItem.innerText = `Currently based in ${personCity}, ${personCountry}`;
 		const personContactItem = document.createElement("li");
+		personContactItem.className = "personContactItem";
 		personCardList.append(personContactItem);
 		personContactItem.innerText = `Can be contacted at ${personEmail}`;
 		const personPictureItem = document.createElement("img");
+		personPictureItem.className = "personPictureItem";
 		personCardList.append(personPictureItem);
 		personPictureItem.src = `${personPicture}`;
 		counter += 1;
