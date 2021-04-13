eac2e18de42b5b41ce7647b3d817ed6a07458959
James Walden <prussian1740@gmail.com>
2021-04-13 12:19:33 -0400

    layout things

--- week_6/frontendproject/JavaScript/index.js
+++ week_6/frontendproject/JavaScript/index.js
@@ -49,3 +49,54 @@ anchorCompanies.setAttribute("href", "exhibitors.html");
 anchorCompanies.innerText = "Exhibitors";
 anchorCompanies.id = "anchorCompanies";
 headerDiv.append(anchorCompanies);
+
+//--Event Listeners--//
+
+const grabHomeLink = document.querySelector("#containerDiv-1");
+grabHomeLink.addEventListener("mousedown", () => {
+	getCreators();
+});
+
+//--Get Artisan(s) Function--//
+
+const getCreators = async () => {
+	//--Fetch Creator Data--//
+	const getCreatorData = await fetch("https://randomuser.me/api/?results=10");
+	const peopleDataJson = await getCreatorData.json();
+	console.log(peopleDataJson);
+	//--Featured Creator Card(s)--//
+	let counter = 0;
+	for (let creator of peopleDataJson.results) {
+		//-Div-//
+		const featuredCreatorDiv = document.createElement("div");
+		featuredCreatorDiv.className = "featuredCreatorDiv";
+		featuredCreatorDiv.setAttribute("id", `creator-${counter}`);
+		containerDiv.append(featuredCreatorDiv);
+		//-Element InnerText-//
+		let personFirstName = peopleDataJson.results[counter].name.first;
+		let personLastName = peopleDataJson.results[counter].name.last;
+		let personCity = peopleDataJson.results[counter].location.city;
+		let personCountry = peopleDataJson.results[counter].location.country;
+		let personEmail = peopleDataJson.results[counter].email;
+		let personPicture = peopleDataJson.results[counter].picture.large;
+		//-Creator Name Header-//
+		const personName = document.createElement("h3");
+		personName.id = "personName";
+		featuredCreatorDiv.append(personName);
+		personName.innerText = `${personLastName}, ${personFirstName}`;
+		//-Creator Card List-//
+		const personCardList = document.createElement("ul");
+		personCardList.id = "personCity";
+		featuredCreatorDiv.append(personCardList);
+		const cityListItem = document.createElement("li");
+		personCardList.append(cityListItem);
+		cityListItem.innerText = `Currently based in ${personCity}, ${personCountry}`;
+		const personContactItem = document.createElement("li");
+		personCardList.append(personContactItem);
+		personContactItem.innerText = `Can be contacted at ${personEmail}`;
+		const personPictureItem = document.createElement("img");
+		personCardList.append(personPictureItem);
+		personPictureItem.src = `${personPicture}`;
+		counter += 1;
+	}
+};
