ac0574655d7d0b60d4be1a47d64fe969676efb90
James Walden <prussian1740@gmail.com>
2021-04-14 12:30:34 -0400

    working on dynamic scrolling

--- week_6/frontendproject/JavaScript/market.js
+++ week_6/frontendproject/JavaScript/market.js
@@ -51,14 +51,17 @@ headerDiv.append(anchorCompanies);
 //--Event Listeners--//
 
 document.addEventListener("DOMContentLoaded", () => {
-	getProducts();
+	getProducts1();
+	getProducts2();
+	getProducts3();
+	getProducts4();
 });
 
 //--Get Product(s) Function--//
 
-const getProducts = async () => {
+const getProducts1 = async () => {
 	const getProductData = await fetch(
-		"https://fakestoreapi.com/products?limit=5"
+		"https://fakestoreapi.com/products?limit=4"
 	);
 	const getProductDataJson = await getProductData.json();
 	console.log(getProductDataJson);
@@ -103,7 +106,163 @@ const getProducts = async () => {
 		// productDiv.append(productDescriptItem);
 		const productPriceItem = document.createElement("li");
 		productPriceItem.className = "productPriceItem";
-		productPriceItem.innerText = `$${productPrice}`;
+		productPriceItem.innerText = `>$${productPrice}`;
+		productDiv.append(productPriceItem);
+		counter += 1;
+	}
+};
+const getProducts2 = async () => {
+	const getProductData = await fetch(
+		"https://fakestoreapi.com/products?limit=4"
+	);
+	const getProductDataJson = await getProductData.json();
+	console.log(getProductDataJson);
+	//-Create Product Card-//
+	let counter = 0;
+	for (let product of getProductDataJson) {
+		//-Div-//
+
+		const productDiv = document.createElement("div");
+		productDiv.className = "productDiv";
+		productDiv.setAttribute("id", `counter-${counter}`);
+		containerDiv2.append(productDiv);
+
+		//-Element InnerText-//
+
+		let productTitle = getProductDataJson[counter].title;
+		let productIMG = getProductDataJson[counter].image;
+		let productDescript = getProductDataJson[counter].description;
+		let productPrice = getProductDataJson[counter].price;
+
+		//-Product Name Header-//
+
+		const productName = document.createElement("h5");
+		productName.className = "productName";
+		productName.innerText = `${productTitle}`;
+		productDiv.append(productName);
+
+		//-Product Card List-//
+
+		const productCardList = document.createElement("ul");
+		productCardList.className = "productCardList";
+		productDiv.append(productCardList);
+		const productIMGItem = document.createElement("img");
+		productIMGItem.className = "productIMGItem";
+		productIMGItem.width = "100";
+		productIMGItem.height = "100";
+		productIMGItem.src = `${productIMG}`;
+		productDiv.append(productIMGItem);
+		// const productDescriptItem = document.createElement("li");
+		// productDescriptItem.className = "productDescriptItem";
+		// productDescriptItem.innerText = `${productDescript}`;
+		// productDiv.append(productDescriptItem);
+		const productPriceItem = document.createElement("li");
+		productPriceItem.className = "productPriceItem";
+		productPriceItem.innerText = `>$${productPrice}`;
+		productDiv.append(productPriceItem);
+		counter += 1;
+	}
+};
+const getProducts3 = async () => {
+	const getProductData = await fetch(
+		"https://fakestoreapi.com/products?limit=4"
+	);
+	const getProductDataJson = await getProductData.json();
+	console.log(getProductDataJson);
+	//-Create Product Card-//
+	let counter = 0;
+	for (let product of getProductDataJson) {
+		//-Div-//
+
+		const productDiv = document.createElement("div");
+		productDiv.className = "productDiv";
+		productDiv.setAttribute("id", `counter-${counter}`);
+		containerDiv3.append(productDiv);
+
+		//-Element InnerText-//
+
+		let productTitle = getProductDataJson[counter].title;
+		let productIMG = getProductDataJson[counter].image;
+		let productDescript = getProductDataJson[counter].description;
+		let productPrice = getProductDataJson[counter].price;
+
+		//-Product Name Header-//
+
+		const productName = document.createElement("h5");
+		productName.className = "productName";
+		productName.innerText = `${productTitle}`;
+		productDiv.append(productName);
+
+		//-Product Card List-//
+
+		const productCardList = document.createElement("ul");
+		productCardList.className = "productCardList";
+		productDiv.append(productCardList);
+		const productIMGItem = document.createElement("img");
+		productIMGItem.className = "productIMGItem";
+		productIMGItem.width = "100";
+		productIMGItem.height = "100";
+		productIMGItem.src = `${productIMG}`;
+		productDiv.append(productIMGItem);
+		// const productDescriptItem = document.createElement("li");
+		// productDescriptItem.className = "productDescriptItem";
+		// productDescriptItem.innerText = `${productDescript}`;
+		// productDiv.append(productDescriptItem);
+		const productPriceItem = document.createElement("li");
+		productPriceItem.className = "productPriceItem";
+		productPriceItem.innerText = `>$${productPrice}`;
+		productDiv.append(productPriceItem);
+		counter += 1;
+	}
+};
+const getProducts4 = async () => {
+	const getProductData = await fetch(
+		"https://fakestoreapi.com/products?limit=4"
+	);
+	const getProductDataJson = await getProductData.json();
+	console.log(getProductDataJson);
+	//-Create Product Card-//
+	let counter = 0;
+	for (let product of getProductDataJson) {
+		//-Div-//
+
+		const productDiv = document.createElement("div");
+		productDiv.className = "productDiv";
+		productDiv.setAttribute("id", `counter-${counter}`);
+		containerDiv4.append(productDiv);
+
+		//-Element InnerText-//
+
+		let productTitle = getProductDataJson[counter].title;
+		let productIMG = getProductDataJson[counter].image;
+		let productDescript = getProductDataJson[counter].description;
+		let productPrice = getProductDataJson[counter].price;
+
+		//-Product Name Header-//
+
+		const productName = document.createElement("h5");
+		productName.className = "productName";
+		productName.innerText = `${productTitle}`;
+		productDiv.append(productName);
+
+		//-Product Card List-//
+
+		const productCardList = document.createElement("ul");
+		productCardList.className = "productCardList";
+		productDiv.append(productCardList);
+		const productIMGItem = document.createElement("img");
+		productIMGItem.className = "productIMGItem";
+		productIMGItem.width = "100";
+		productIMGItem.height = "100";
+		productIMGItem.src = `${productIMG}`;
+		productDiv.append(productIMGItem);
+		// const productDescriptItem = document.createElement("li");
+		// productDescriptItem.className = "productDescriptItem";
+		// productDescriptItem.innerText = `${productDescript}`;
+		// productDiv.append(productDescriptItem);
+		const productPriceItem = document.createElement("li");
+		productPriceItem.className = "productPriceItem";
+		productPriceItem.innerText = `>$${productPrice}`;
 		productDiv.append(productPriceItem);
 		counter += 1;
 	}
