489896d292d577bcba46d85bd7e9d5570f45b67b
James Walden <78229982+jwalden2019@users.noreply.github.com>
2021-05-25 23:43:04 -0400

    film_website

--- week_12/day_1/OMDBProject/film_website/src/components/Films.jsx
+++ week_12/day_1/OMDBProject/film_website/src/components/Films.jsx
@@ -4,9 +4,9 @@ import "../styles/Films.css";
 export default function Films({ film }) {
     return (
         <div className="films__getFilmDisplay">
-            <h4 className="film__title">{film.Title}</h4>
-            <p className="film__standard">{film.Year}</p>
+        <center>
             <img className="film__Poster" src={film.Poster} alt=""></img>
+        </center>
             </div>
     )
 }
